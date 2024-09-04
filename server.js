28a1283bb949c7957dca45af9ebe70910995104e
malay77patra <malay77patra@gmail.com>
2024-09-04 14:33:04 +0530

    test

--- server.js
+++ server.js
@@ -7,24 +7,26 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res, next) => {
-    let requestedPath = path.join(__dirname, 'build', req.path);
-
-    if (path.extname(requestedPath) === '') {
-        requestedPath += '.html';
-    }
-    res.status(200).send(requestedPath);
-
-    if (path.extname(requestedPath) !== '.html') {
-        return res.status(404).send('404 Not Found');
-    }
-
-
-    if (fs.existsSync(requestedPath)) {
-        return res.sendFile(requestedPath);
-    } else {
-        res.status(404).send('404 Not Found');
-    }
+    res.status(200).send("ok, good");
 });
+// app.get('*', (req, res, next) => {
+//     let requestedPath = path.join(__dirname, 'build', req.path);
+
+//     if (path.extname(requestedPath) === '') {
+//         requestedPath += '.html';
+//     }
+
+//     if (path.extname(requestedPath) !== '.html') {
+//         return res.status(404).send('404 Not Found');
+//     }
+
+
+//     if (fs.existsSync(requestedPath)) {
+//         return res.sendFile(requestedPath);
+//     } else {
+//         res.status(404).send('404 Not Found');
+//     }
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
